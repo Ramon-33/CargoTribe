d6fa7b6d806853b6195c2bd521133bc41219861d
Ramon-33 <ramon_kolk@hotmail.com>
2024-04-30 21:41:56 +0200

    update

--- pages/components/apps/agents copy.js
+++ pages/components/apps/agents copy.js
@@ -1,37 +1,37 @@
-import Seo from '@/shared/layout-components/seo/seo';
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import dynamic from 'next/dynamic';
-const DataTablesCom = dynamic(()=>import('@/shared/data/table/datatable/agents-table'), { ssr: false })
+// import Seo from '@/shared/layout-components/seo/seo';
+// import React from 'react';
+// import { Button } from 'react-bootstrap';
+// import dynamic from 'next/dynamic';
+// const DataTablesCom = dynamic(()=>import('@/shared/data/table/datatable/agents-table'), { ssr: false })
 
-const Agents = () => (
-  <div>
-    <Seo title={"Agents"}/>
-    {/* <!-- breadcrumb --> */}
-    <div className="breadcrumb-header justify-content-between">
-      <div className="left-content">
-        <span className="main-content-title mg-b-0 mg-b-lg-1">AGENTS</span>
-      </div>
-      <div className="justify-content-center mt-2">
-      <Button variant="" type="button" className="btn btn-primary" onClick={handleAddNewAgent}>
-            <i className="fe fe-plus me-1"></i> Add New Agent
-          </Button>
-      </div>
-    </div>
-    {/* <!-- /breadcrumb --> */}
+// const Agents = () => (
+//   <div>
+//     <Seo title={"Agents"}/>
+//     {/* <!-- breadcrumb --> */}
+//     <div className="breadcrumb-header justify-content-between">
+//       <div className="left-content">
+//         <span className="main-content-title mg-b-0 mg-b-lg-1">AGENTS</span>
+//       </div>
+//       <div className="justify-content-center mt-2">
+//       <Button variant="" type="button" className="btn btn-primary" onClick={handleAddNewAgent}>
+//             <i className="fe fe-plus me-1"></i> Add New Agent
+//           </Button>
+//       </div>
+//     </div>
+//     {/* <!-- /breadcrumb --> */}
 
-    {/* <!-- row --> */}
+//     {/* <!-- row --> */}
       
-      <DataTablesCom/>
-      {/* <!-- row closed --> */}
-    </div>
-);
+//       <DataTablesCom/>
+//       {/* <!-- row closed --> */}
+//     </div>
+// );
 
 
-Agents.propTypes = {};
+// Agents.propTypes = {};
 
-Agents.defaultProps = {};
+// Agents.defaultProps = {};
 
-Agents.layout = "Contentlayout"
+// Agents.layout = "Contentlayout"
 
-export default Agents;
+// export default Agents;
